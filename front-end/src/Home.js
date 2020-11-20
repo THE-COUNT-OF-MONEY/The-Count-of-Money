5d957df00f6fc5e8162589f8515351f1c289822d
Iyad <iyad.callas@epitech.eu>
2020-11-20 19:32:06 +0100

    Update Homepage

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,13 +1,10 @@
 import React from "react"
-import Button from "@material-ui/core/Button"
+import Navbar from "./Navbar"
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <Button variant="contained" color="secondary">
-                Logout
-            </Button>
+            <Navbar />
         </div>
     )
 }
