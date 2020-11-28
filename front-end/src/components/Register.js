6522f47d888d7a05781b19c9143134907ed65b73
jatin-bhatia1 <bhatia.jatin1997@gmail.com>
2020-11-28 15:56:16 +0100

    Auth with google provider

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -6,7 +6,9 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {loginPost} from '../services/ApiFunctions'
+import {RegisterPost} from '../services/ApiFunctions'
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
 
 const useStyles = theme => ({
   paper: {
@@ -33,20 +35,35 @@ class Register extends Component{
     super();
 
     this.state = {
-      firstName: '',
-      lastName: '',
+      firstname: '',
+      lastname: '',
       email: '',
-      password: ''
+      password: '',
+      errorMessage: '',
+      redirect: false
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.register = this.register.bind(this);
     this.onChange = this.onChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    if(this.state.email && this.state.password)
+  }
+  register()
+  {
+    if(this.state.firstname && this.state.lastname && this.state.email && this.state.password )
     {
-      loginPost(this.state);
+      let data = {'firstname' : this.state.firstname, 'lastname': this.state.lastname, 'email': this.state.email, 'password': this.state.password}
+      let dataJson = JSON.stringify(data)
+      RegisterPost(dataJson).then((result)=>{
+        if(result === 'created')
+        {
+          this.setState({redirect: true});
+        }
+        else{
+          this.setState({errorMessage: result});
+        }
+      })
      }
   }
    validateForm() {
@@ -56,9 +73,19 @@ class Register extends Component{
     onChange(e){
     this.setState({[e.target.name]: e.target.value});
     }
+    renderErrrorMessage(){
+      if(this.state.errorMessage !== ''){
+        return <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+          {this.state.errorMessage} — <strong>check it out!</strong>
+      </Alert>
+      }
+      else return '';
+    }
 
     render () {
       const {classes} = this.props;
+      
       return (
         <Container component="main" maxWidth="xs">
           <CssBaseline />
@@ -66,18 +93,19 @@ class Register extends Component{
             <Typography component="h1" variant="h5">
               Register
             </Typography>
-            <form className={classes.form} noValidate>
+            <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
                   <TextField
                     autoComplete="fname"
-                    name="firstName"
+                    name="firstname"
                     variant="outlined"
                     required
                     fullWidth
-                    id="firstName"
+                    id="firstname"
                     label="First Name"
                     autoFocus
+                    onChange={this.onChange}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -85,10 +113,11 @@ class Register extends Component{
                     variant="outlined"
                     required
                     fullWidth
-                    id="lastName"
+                    id="lastname"
                     label="Last Name"
-                    name="lastName"
+                    name="lastname"
                     autoComplete="lname"
+                    onChange={this.onChange}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -100,6 +129,7 @@ class Register extends Component{
                     label="Email Address"
                     name="email"
                     autoComplete="email"
+                    onChange={this.onChange}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -112,10 +142,11 @@ class Register extends Component{
                     type="password"
                     id="password"
                     autoComplete="current-password"
+                    onChange={this.onChange}
                   />
                 </Grid>
               </Grid>
-              <Button
+              <Button onClick={this.register}
                 fullWidth
                 variant="contained"
                 color="primary"
@@ -124,6 +155,9 @@ class Register extends Component{
                 Register
               </Button>
             </form>
+            <div>              
+              {this.renderErrrorMessage()}
+            </div>
           </div>
         </Container>
       );
