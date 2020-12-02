3b39923f833339257079fa7ec8d1dc4199d9d28c
sabri.ouaked <sabri.ouaked@epitech.eu>
2020-12-02 20:46:34 +0100

    [-] remove all paths

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom"
-import Home from "./components/Home"
 import Login from "./components/Login"
 import Dashboard from "./components/Dashboard";
 import Register from "./components/Register"
-
 import {Currencies} from './ressources/currencies/CurrenciesWidget'
 import {Feeds} from './ressources/feeds/FeedsWidget'
-
-// import {Dashboard} from './ressources/dashboard/Dashboard';
-// import {Currencies} from './ressources/currencies/CurrenciesWidget';
-// import {Feeds} from "./ressources/feeds/FeedsWidget.js";
 
 const App = () => {
   return (
@@ -18,7 +12,6 @@ const App = () => {
       <div>
         <Route exact path="/feeds" component={ Feeds } />
         <Route exact path="/currencies" component={ Currencies } />
-        {/* <Route exact path="/" component={Home} /> */}
         {/* <Route exact path="/login" component={Login} /> */}
         <Route exact path="/register" component={Register} />
         <Route exact path="/" component={Dashboard} />
