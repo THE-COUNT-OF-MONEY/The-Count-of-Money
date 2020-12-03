9c727073aa99135c7f7351e9df8eb0e36ba4668d
Iyad <iyad.callas@epitech.eu>
2020-12-03 16:57:08 +0100

    Profile page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Login from "./components/Login"
 import Register from "./components/Register"
+import Profile from "./components/Profile"
 import {Currencies} from './ressources/currencies/CurrenciesWidget'
 import {Feeds} from './ressources/feeds/FeedsWidget'
 import Navbar from "./components/Navbar";
@@ -22,6 +23,7 @@ const App = () => {
               <Route exact path="/feeds" component={ Feeds } />
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/profile" component={Profile} />
               <Route exact path="/users" component={Users} />
             </Grid>
           
