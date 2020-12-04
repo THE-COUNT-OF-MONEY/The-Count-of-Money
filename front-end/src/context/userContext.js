841c4e82b4be40782042f076e131ef11d9132a8b
sabri.ouaked <sabri.ouaked@epitech.eu>
2020-12-04 15:02:16 +0100

    [fix] user erro

--- front-end/src/context/userContext.js
+++ front-end/src/context/userContext.js
@@ -8,16 +8,18 @@ export const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  localStorage.token = null;
   useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
 
             Api.getProfile().then((res) => {
                 if (res.status === 200) {
-                    if (res.data !== undefined || res.data.content !== undefined){
+                    if (res.data !== undefined && res.data.content !== undefined){
                         const userData = res.data.content.user;
                         setUser(res.data.content.user)
                     }
                 }
+        
             });
         });
   }, []);
