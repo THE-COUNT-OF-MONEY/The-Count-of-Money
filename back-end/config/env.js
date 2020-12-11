78fb1377378878fc04adf90740eb0d3414e70081
sabri.ouaked <sabri.ouaked@epitech.eu>
2020-12-11 17:15:25 +0100

    [+] database dynamic conf

--- back-end/config/env.js
+++ back-end/config/env.js
@@ -11,9 +11,33 @@ function isInProduction() {
 module.exports = {
 
     getVariables() {
+
+        const devConfig = {
+            apiKey: 'AIzaSyDndtZWxGbteNSLobZ5O2CmS1fyX-2sAGg',
+            authDomain: 'count-of-money-dev.firebaseapp.com',
+            databaseURL: 'https://count-of-money-dev.firebaseio.com',
+            projectId: 'count-of-money-dev',
+            storageBucket: 'count-of-money-dev.appspot.com',
+            messagingSenderId: '516320172645',
+            appId: '1:516320172645:web:cfb490ef4bf66a96d3848c',
+            measurementId: 'G-5N4WKF17VF'
+        };
+
+        const prodConfig = {
+            apiKey: "AIzaSyDdbAo9cJeQ8qUsCdutPTuQEreb7SedvbY",
+            authDomain: "count-of-money-prod.firebaseapp.com",
+            databaseURL: "https://count-of-money-prod.firebaseio.com",
+            projectId: "count-of-money-prod",
+            storageBucket: "count-of-money-prod.appspot.com",
+            messagingSenderId: "226050228862",
+            appId: "1:226050228862:web:646463b711ffca90ac0e2f",
+            measurementId: "G-JH3JRJ197S"
+          };
+
         let database = {
             credentials: isInProduction() ? process.env.CREDENTIALS_PROD : process.env.CREDENTIALS_DEV,
-            databaseUrl: isInProduction() ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL_DEV
+            databaseUrl: isInProduction() ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL_DEV,
+            config: isInProduction() ? prodConfig : devConfig
         }
 
         return {
--- back-end/services/database.js
+++ back-end/services/database.js
@@ -5,19 +5,9 @@ var admin = require('firebase-admin');
 require('firebase/auth');
 require('firebase/firestore');
 
-exports.initialize = (credentials, databaseUrl) => {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyDndtZWxGbteNSLobZ5O2CmS1fyX-2sAGg',
-    authDomain: 'count-of-money-dev.firebaseapp.com',
-    databaseURL: 'https://count-of-money-dev.firebaseio.com',
-    projectId: 'count-of-money-dev',
-    storageBucket: 'count-of-money-dev.appspot.com',
-    messagingSenderId: '516320172645',
-    appId: '1:516320172645:web:cfb490ef4bf66a96d3848c',
-    measurementId: 'G-5N4WKF17VF'
-  };
+exports.initialize = (credentials, config, databaseUrl) => {
 
-  firebase.default.initializeApp(firebaseConfig);
+  firebase.default.initializeApp(config);
 
   admin.initializeApp({
     credential: admin.credential.cert(credentials),
