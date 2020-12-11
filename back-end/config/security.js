220567bd4877f292f8cf9872e7d90ff5dbff4b69
Mehdi Rhoulam <mehdi.rhoulam@epitech.eu>
2020-12-11 23:17:37 +0100

    fonctionnement Csrf Token plus cookie, Cors localhost etc etc, exposedheader cookies

--- back-end/config/security.js
+++ back-end/config/security.js
@@ -1,31 +1,49 @@
 const csrf = require('csurf');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-
 exports.initializeCSRF = (app, router) => {
-  
   var csrfProtection = csrf({ cookie: true });
   var parseForm = bodyParser.json({ extended: false });
-  
+
   app.use(cookieParser());
 
   app.get('/csrf', parseForm, csrfProtection, function (req, res) {
-      const response = {
-          'message': 'csrfToken successfully gotten',
-          'content': {
-              'csrfToken': req.csrfToken(),
-          }
+    const response = {
+      message: 'csrfToken successfully gotten',
+      content: {
+        csrfToken: req.csrfToken()
       }
-      res.send(response);
+    };
+    res.send(response);
   });
 
-  app.post("*", parseForm, csrfProtection, function (req, res, next) {
+  app.post('*', parseForm, csrfProtection, function (req, res, next) {
     next();
   });
-}
+
+  app.post('/test', parseForm, csrfProtection, function (req, res) {
+    const response = {
+      message: 'Nice you came inside!!!'
+    };
+    res.send(response);
+  });
+};
 
 exports.initializeCORS = (app) => {
-  app.use(cors());
-}
+  const corsOptions = {
+    //To allow requests from client
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'http://127.0.0.1',
+      'http://127.0.0.1:3000',
+      'http://mydomain.com'
+    ],
+    credentials: true,
+    exposedHeaders: ['set-cookie']
+  };
+
+  app.use(cors(corsOptions));
+};
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,77 @@
-import React from "react";
-import {BrowserRouter as Router} from "react-router-dom"
-import Navbar from "./components/Navbar";
-import { Grid } from "@material-ui/core";
-import { UserProvider } from "./context/userContext";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Grid } from '@material-ui/core';
+import { UserProvider } from './context/userContext';
 import { Routes } from './Routes';
-import { LimitProvider } from "./context/limitContext";
-import { CsrfProvider } from "./context/csrfContext";
+import { LimitProvider } from './context/limitContext';
+import { CsrfProvider } from './context/csrfContext';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const App = () => {
-  
+  var config = {
+    method: 'get',
+    url: 'http://localhost:8000/csrf',
+    withCredentials: true,
+    headers: {}
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(response.data.content.csrfToken);
+      console.log('Cookie == ? ', Cookies.get('_csrf'));
+
+      // axios
+      //   .get('http://localhost:8000/test', { withCredentials: true })
+      //   .then((res) => console.log('Nice', res.data))
+      //   .catch((err) => {
+      //     console.log('not hit');
+      //     /* not hit since no 401 */
+      //   });
+
+      axios('http://localhost:8000/test', {
+        method: 'post',
+        data: {
+          _csrf: response.data.content.csrfToken
+        },
+        withCredentials: true
+      }).then(function (res) {
+        console.log('MOTHERFKIN WOW ????', res);
+      });
+
+      // axios('http://localhost:8000/test', {
+      //   method: 'POST',
+      //   body: {
+      //     _csrf: response.data.content.csrfToken,
+      //     favoriteColor: 'ReallyMyNiG?'
+      //   },
+      //   withCredentials: true
+      // }).then(function (res) {
+      //   alert('GGGGG!');
+      // });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   return (
     <div>
       <Grid container direction="column" spacing={2}>
-          <Router>
-            <CsrfProvider>
-              <LimitProvider>
-                <UserProvider>
+        <Router>
+          <CsrfProvider>
+            <LimitProvider>
+              <UserProvider>
+                <Grid item>
+                  <Navbar></Navbar>
+                </Grid>
 
-                  <Grid item>
-                    <Navbar></Navbar>
-                  </Grid>
-
-                  <Routes/>
-                </UserProvider>
-              </LimitProvider>
-            </CsrfProvider>
-          </Router>
+                <Routes />
+              </UserProvider>
+            </LimitProvider>
+          </CsrfProvider>
+        </Router>
       </Grid>
-
     </div>
   );
 };
